5f3d8de13bc9beb0138f6523af55fcd8f3ca184c
SaraLoMoutinho <saralomoutinho@gmail.com>
2016-03-16 16:49:55 +0200

    get involved landing page

--- prototype/index.js
+++ prototype/index.js
@@ -53,6 +53,10 @@ app.get('/pages/win', function (req, res) {
   res.render('pages/win', {data: data})
 })
 
+app.get('/pages/involved', function (req, res) {
+  res.render('pages/involved', {data: data})
+})
+
 var server = app.listen(3000, function () {
   console.log('Server running at http://localhost:' + server.address().port)
 })
